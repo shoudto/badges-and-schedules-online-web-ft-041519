b362427268039fb97842893a480fb71db8308da9
Oscar Shoudt <shoudto@gmail.com>
2019-04-19 14:27:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,12 @@
 def badge_maker(name)
+  
   return "Hello, my name is #{name}."
+  
 end 
 
 
 def batch_badge_creator(attendees)
+  
   attendees.each{ |attendee| puts "Hello, my name is #{attendee}"}
+  
 end 
