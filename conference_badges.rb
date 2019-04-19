b84e1ac21773b0bb58dbb7fd1419525404b85d39
Oscar Shoudt <shoudto@gmail.com>
2019-04-19 15:39:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,7 @@ end
 def batch_badge_creator(attendees)
   badge_array = []
   attendees.each do |attendee|
-   badge_array << "Hello, my name is #{attendee}"
+   badge_array << "Hello, my name is #{attendee}."
   end
   badge_array
 end 
